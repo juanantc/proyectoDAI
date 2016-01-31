b95d0e2f7e61582f2ecef659048331f30783cc0f
Juan Antonio Moraleda Ocón <juanantc@gmail.com>
2016-01-31 23:22:44 +0100

    Update settings.py

--- proyectoDAI/settings.py
+++ proyectoDAI/settings.py
@@ -92,6 +92,7 @@ TEMPLATE_DIRS = (
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_PATH = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATIC_URL = '/static/' # You may find this is already defined as such.
 
